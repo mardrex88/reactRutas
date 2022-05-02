3c6458d364dff3d25121bc9f7aef205db1214579
Marlon Vargas <mardrex88@gmail.com>
2022-05-02 17:34:57 -0500

    add  react-router-dom ---- se implementa la ultima version 6.3 de la dependencia ya que la version del video es desaactualizada

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <h1>Hola bienvenido a las rutas!</h1>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        </div>
+     <Routes>     
+        <Route path="/" element={
+        <h1>Hola soy el Home</h1>
+        } />
+         <Route path="/about" element={
+        <h1>Hola soy el About</h1>
+        } />
+        <Route path="/users" element={
+        <h1>Hola soy el Users</h1>
+        } />
+    </Routes>
+  </div>
   );
 }
 
